f81a0d1973c4708b72c9ca05971a42e546a9b8df
AMDIvailo <ipanamski@gmail.com>
2015-10-24 14:47:41 +0300

    Added calculator command in the help.

--- commands/help.cpp
+++ commands/help.cpp
@@ -3,4 +3,5 @@ void help()
 {
 	std::cout << "*Help -- Shows this help." <<std::endl;
 	std::cout << "*About -- Shows details about the program and license." <<std::endl;
+	std::cout << "*Calculator -- Starts the calculator mode." <<std::endl;
 }
