3ce58a19b6d59187781b666aa791772d295151b7
AMDIvailo <ipanamski@gmail.com>
2015-10-26 22:38:09 +0200

    Added some more help for the new commands.

--- commands/help.cpp
+++ commands/help.cpp
@@ -5,4 +5,6 @@ void help()
 	std::cout << "*About -- Shows details about the program and license." <<std::endl;
 	std::cout << "*Calculator -- Starts the calculator mode." <<std::endl;
 	std::cout << "*Version -- Shows the version of the MultyTerminal." <<std::endl;
+	std::cout << "*Exit -- Exits the MultyTerminal." <<std::endl;
+	std::cout << "*Timer [time in seconds] -- Starts a timer." <<std::endl;
 }
